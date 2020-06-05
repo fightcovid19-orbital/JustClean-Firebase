67607c2953d89331661b3ce11e1b05eaa1476aec
WayneTan <waynetan2000@gmail.com>
2020-06-05 16:52:14 +0800

    get all the cleaners

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,15 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.getCleaners = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('cleaners').get()
+        .then(data => {
+            let cleaners = [];
+            data.forEach(doc => {
+                cleaners.push(doc.data());
+            });
+            return res.json(cleaners);
+        })
+        .catch(err => console.error(err))
+})
